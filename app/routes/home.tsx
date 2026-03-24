96bf4c2b1b9b99511cf9dea6188c620c1e74a1bc
Vaibhav <vaibhav.pardeshi@atomberg.com>
2026-03-25 01:51:21 +0530

    first commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,13 @@
+import type { Route } from "./+types/home";
+import NavBar from "../../components/NavBar";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <NavBar/>
+}
